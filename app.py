479e519961ae5595be1f9f55458df56b97e3a2d9
arturolumev <edwin.lume@tecsup.edu.pe>
2023-11-30 11:53:52 -0500

    cambios en app

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import logging
 
 import csv
 from math import sqrt
-from builtins import zip
+from builtins import os
+
+### 10M
+import fileinput
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import cityblock
 
 option_a = os.getenv('OPTION_A', "Persona 1")
 option_b = os.getenv('OPTION_B', "Persona 2")
@@ -74,7 +80,7 @@ def recommend(username, users):
     return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
 
 #print( recommend('Hailey', users))
-result = recommend('Hailey', users)
+# result = recommend('Hailey', users)
 
 ################################################################
 
@@ -158,10 +164,109 @@ def recommend(username, users):
         print(f"Película: {movie}, Puntuación: {rating}")
 
 # Ejemplo de uso
-users2 = load_users_from_csv('ratings.csv')
-username = '1'  # Replace this with the desired user ID for recommendations
-recommend(username, users2)
-print(pearson_correlation(users2['1'], users2['5']))
+#users2 = load_users_from_csv('ratings.csv')
+#username = '1'  # Replace this with the desired user ID for recommendations
+# recommend(username, users2)
+# print(pearson_correlation(users2['1'], users2['5']))
+
+################################################################
+
+# USO DE 1m DATOS
+
+# Ruta al archivo que quieres modificar
+archivo = 'ratings.dat'
+
+# Leer el contenido del archivo
+with open(archivo, 'r') as file:
+    lines = file.readlines()
+
+# Modificar el contenido reemplazando '::' por '\t'
+modified_lines = [line.replace('::', '\t') for line in lines]
+
+# Sobrescribir el archivo con los cambios
+with open(archivo, 'w') as file:
+    file.writelines(modified_lines)
+
+# Convert MovieLens data to binary using numpy_to_binary function
+def movie_lens_to_binary(input_file, output_file):
+    # Load MovieLens data using Pandas
+    ratings = pd.read_csv(input_file, sep='\t', header=None,
+                          names=['userId', 'movieId', 'rating', 'rating_timestamp'])
+    # Convert to NumPy array
+    np_data = np.array(ratings[['userId', 'movieId', 'rating']])
+    # Write to binary file
+    with open(output_file, "wb") as bin_file:
+        bin_file.write(np_data.astype(np.int32).tobytes())
+        
+movie_lens_to_binary('ratings.dat', 'output_binary.bin')
+
+#it takes 32 seconds
+#comparate
+
+def computeNearestNeighbor(dataframe, target_user, distance_metric=cityblock):
+    distances = np.zeros(len(dataframe))  # Initialize a NumPy array
+    # Iterate over each row (user) in the DataFrame
+    for i, (index, row) in enumerate(dataframe.iterrows()):
+        if index == target_user:
+            continue  # Skip the target user itself
+        # Calculate the distance between the target user and the current user
+        distance = distance_metric(dataframe.loc[target_user].fillna(0), row.fillna(0))
+        distances[i] = distance
+    # Get the indices that would sort the array, and then sort the distances accordingly
+    sorted_indices = np.argsort(distances)
+    sorted_distances = distances[sorted_indices]
+    return list(zip(dataframe.index[sorted_indices], sorted_distances))
+
+def binary_to_pandas_with_stats(bin_file, num_rows=10):
+    # Read binary data into NumPy array using context manager
+    with open(bin_file, 'rb') as f:
+        binary_data = f.read()
+    # Convert binary data back to NumPy array
+    np_data = np.frombuffer(binary_data, dtype=np.int32).reshape(-1, 3)  # Assuming 3 columns
+    # Convert NumPy array to Pandas DataFrame
+    df = pd.DataFrame(np_data, columns=['userId', 'movieId', 'rating'])
+    return df
+
+def consolidate_data(df):
+    # Group by 'userId' and 'movieId' and calculate the mean of 'rating'
+    consolidated_df = df.groupby(['userId', 'movieId'])['rating'].mean().unstack()
+    return consolidated_df
+
+def recommend_movies_from_binary(username, df):
+    nearest = computeNearestNeighbor(consolidated_df, target_user_id)
+    movies_rated_by_user = set(df[df['userId'] == username]['movieId'])
+
+    recommendations = {}
+    for neighbor, distance in nearest[1:6]:  # Excluye el usuario mismo
+        neighbor_movies = set(df[df['userId'] == neighbor]['movieId'])
+
+        for movie in neighbor_movies:
+            if movie not in movies_rated_by_user:
+                if movie not in recommendations:
+                    recommendations[movie] = 1
+                else:
+                    recommendations[movie] += 1
+
+    sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+
+    print(f"Las recomendaciones de películas para {username} son:")
+    for movie, rating in sorted_recommendations[:5]:
+        print(f"Película: {movie}, Puntuación: {rating}")
+
+df = binary_to_pandas_with_stats('output_binary.bin', num_rows=10)
+
+# Consolidate data
+consolidated_df = consolidate_data(df)
+
+# Example usage
+# Assuming your DataFrame is named 'ratings_df'
+target_user_id = 1
+neighbors = computeNearestNeighbor(consolidated_df, target_user_id)
+# Print the nearest neighbors and their distances
+#print("Nearest Neighbors for User {}: {}".format(target_user_id, neighbors))
+#print(neighbors[1][1])
+
+recommend_movies_from_binary(target_user_id, df)
 
 ################################################################
 
