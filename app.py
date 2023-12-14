4c66d8a8a94c2eda17e6d6865fb22360dded1479
arturolumev <edwin.lume@tecsup.edu.pe>
2023-12-14 11:50:55 -0500

    20 millones

--- app.py
+++ app.py
@@ -16,158 +16,20 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.distance import cityblock
 
+### 20M
+import math
+import pandas as pd
+import dask.dataframe as dd
+
 option_a = os.getenv('OPTION_A', "Persona 1")
 option_b = os.getenv('OPTION_B', "Persona 2")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
 
-users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
-         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
-         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
-         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
-         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
-         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
-        }
-
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
-
-################################
-
-def manhattan(rating1, rating2):
-    """Computes the Manhattan distance. Both rating1 and rating2 are dictionaries
-       of the form {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}"""
-    distance = 0
-    commonRatings = False 
-    for key in rating1:
-        if key in rating2:
-            distance += abs(rating1[key] - rating2[key])
-            commonRatings = True
-    if commonRatings:
-        return distance
-    else:
-        return -1 #Indicates no ratings in common
-
-
-def computeNearestNeighbor(username, users):
-    """creates a sorted list of users based on their distance to username"""
-    distances = []
-    for user in users:
-        if user != username:
-            distance = manhattan(users[user], users[username])
-            distances.append((distance, user))
-    # sort based on distance -- closest first
-    distances.sort()
-    return distances
-
-def recommend(username, users):
-    """Give list of recommendations"""
-    # first find nearest neighbor
-    nearest = computeNearestNeighbor(username, users)[0][1]
-
-    recommendations = []
-    # now find bands neighbor rated that user didn't
-    neighborRatings = users[nearest]
-    userRatings = users[username]
-    for artist in neighborRatings:
-        if not artist in userRatings:
-            recommendations.append((artist, neighborRatings[artist]))
-    # using the fn sorted for variety - sort is more efficient
-    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
-
-#print( recommend('Hailey', users))
-# result = recommend('Hailey', users)
-
-################################################################
-
-################################################################
-# USANDO CSV
-
-def load_users_from_csv(filename):
-    users = {}
-    with open(filename, 'r') as file:
-        csv_reader = csv.DictReader(file)
-
-        for row in csv_reader:
-            userId = row['userId']
-            movieId = row['movieId']
-            rating = float(row['rating'])
-
-            if userId not in users:
-                users[userId] = {}
-            users[userId][movieId] = rating
-
-    return users
-
-def pearson_correlation(rating1, rating2):
-    sum_rating1 = sum(rating1.values())
-    sum_rating2 = sum(rating2.values())
-    sum_sq_rating1 = sum([rating ** 2 for rating in rating1.values()])
-    sum_sq_rating2 = sum([rating ** 2 for rating in rating2.values()])
-
-    n = min(len(rating1), len(rating2))
-    if n == 0:
-        return 0.0  # No common items, return 0 to avoid division by zero
-
-    numerator = 0
-    denominator1 = 0
-    denominator2 = 0
-
-    for item in rating1:
-        if item in rating2:
-            rating1_diff = rating1[item] - (sum_rating1 / n)
-            rating2_diff = rating2[item] - (sum_rating2 / n)
-            numerator += rating1_diff * rating2_diff
-            denominator1 += rating1_diff ** 2
-            denominator2 += rating2_diff ** 2
-
-    denominator = sqrt(denominator1 * denominator2)
-    if not denominator:
-        return 0.0
-    else:
-        return numerator / denominator
-
-def computeNearestNeighbor(username, users):
-    distances = []
-    for user in users:
-        if user != username:
-            distance = pearson_correlation(users[user], users[username])
-            distances.append((distance, user))
-    distances.sort(reverse=True)
-    return distances
-
-def recommend(username, users):
-    nearest = computeNearestNeighbor(username, users)[1:6]
-
-    movies_rated_by_user = set(users[username].keys())
-
-    recommendations = {}
-    for neighbor in nearest:
-        neighbor_username = neighbor[1]
-        neighbor_movies = users[neighbor_username]
-
-        for movie in neighbor_movies:
-            if movie not in movies_rated_by_user:
-                if movie not in recommendations:
-                    recommendations[movie] = neighbor_movies[movie]
-                else:
-                    recommendations[movie] += neighbor_movies[movie]
-
-    sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
-
-    print(f"Las recomendaciones para {username} son:")
-    for movie, rating in sorted_recommendations[:5]:
-        print(f"Película: {movie}, Puntuación: {rating}")
-
-# Ejemplo de uso
-#users2 = load_users_from_csv('ratings.csv')
-#username = '1'  # Replace this with the desired user ID for recommendations
-# recommend(username, users2)
-# print(pearson_correlation(users2['1'], users2['5']))
 
 ################################################################
 
@@ -270,16 +132,243 @@ recommend_movies_from_binary(target_user_id, df)
 
 ################################################################
 
+# 20M
+
+# Init K-vector with correct value based on distance type
+def initVectDist(funName, N):
+    if funName == 'euclidiana' or funName == 'manhattan' or funName == 'euclidianaL' or funName == 'manhattanL':
+        ls = [99999] * N
+    else:
+        ls = [-1] * N
+
+    lu = [None] * N
+    return ls, lu
+
+
+# Keep the closest values, avoiding sort
+def keepClosest(funname, lstdist, lstuser, newdist, newuser, N):
+    if funname == 'euclidiana' or funname == 'manhattan' or funname == 'euclidianaL' or funname == 'manhattanL':
+        count = -1
+        for i in lstdist:
+            count += 1
+            if newdist > i:
+                continue
+            lstdist.insert(count, newdist)
+            lstuser.insert(count, newuser)
+            break
+    else:
+        count = -1
+        for i in lstdist:
+            count += 1
+            if newdist < i:
+                continue
+            lstdist.insert(count, newdist)
+            lstuser.insert(count, newuser)
+            break
+
+    if len(lstdist) > N:
+        lstdist.pop()
+        lstuser.pop()
+    return lstdist, lstuser
+
+def readLargeFile(filename, delim=','):
+  data = pd.read_csv(filename, delimiter=delim, header=None)
+
+  lst = {} # Dictionary
+  j = 0
+  for index, row in data.iterrows():
+      #print(row[0], row[1], row[2])
+      if j != row[0]:
+          j = row[0]
+          tmp = {row[1]:row[2]}
+          lst[row[0]] = tmp
+      else:
+          tmp = lst.get(row[0])
+          tmp[row[1]] = row[2]
+          lst[row[0]] = tmp
+  return lst
+
+def readLargeFileDask(filename, delim=','):
+    # Utiliza assume_missing=True para manejar valores no especificados en la conversión a int64
+    ddf = dd.read_csv(filename, delimiter=delim, header=None, assume_missing=True)
+
+    ddf_grouped = ddf.groupby(0).apply(lambda group: dict(zip(group[1], group[2])), meta=('x', 'f8'))
+
+    result = ddf_grouped.compute().to_dict()
+
+    return result
+
+# Euclidian distance
+def euclidianaL(user1, user2):
+    dist = 0.0
+    count = 0
+    for i in user2:
+        if not (user1.get(i) is None):
+            x = user1.get(i)
+            y = user2.get(i)
+            dist += math.pow(x - y, 2)
+            count += 1
+
+    if count == 0:
+        return 9999.99
+    return math.sqrt(dist)
+
+# Manhattan distance
+def manhattanL(user1, user2):
+    dist = 0.0
+    count = 0
+    for i in user2:
+        if not (user1.get(i) is None):
+            x = user1[i]
+            y = user2[i]
+            dist += abs(x - y)
+            count += 1
+
+    if count == 0:
+        return 9999.99
+    return dist
+
+
+# Cosine distance
+def cosenoL(user1, user2):
+    xy = xx = yy = 0.0
+    count = 0
+    for i in user2:
+        if not (user1.get(i) is None):
+            x = user1[i]
+            y = user2[i]
+            xy += x * y
+            xx += x * x
+            yy += y * y
+            count += 1
+
+    den = math.sqrt(xx) * math.sqrt(yy)
+    if den == 0:
+      return math.nan
+    if count == 0:
+        return math.nan
+    return xy / den  # -1 a +1
+
+
+# Pearson distance
+def pearsonL(user1, user2):
+    xy = zx = zy = n = zxx = zyy = 0.0
+    count = 0
+    for i in user2:
+        if not (user1.get(i) is None):
+            x = user1[i]
+            y = user2[i]
+            xy += x * y
+            zx += x
+            zy += y
+            n += 1
+            zxx += x * x
+            zyy += y * y
+            count += 1
+
+    if n == 0:
+        return math.nan
+    a = xy - (zx * zy) / n
+    b = math.sqrt(zxx - (zx * zx) / n) * math.sqrt(zyy - (zy * zy) / n)
+    if b == 0:
+        return math.nan
+    if count == 0:
+        return math.nan
+    return a / b  # -1 a +1
+# K-Nearest neighbour
+def knn_L(N, distancia, usuario, data):  # N numero de vecinos
+    funName = distancia.__name__
+    print('k-nn', funName)
+
+    listDist, listName = initVectDist(funName, N)
+    nsize = len(data)
+    otherusers = range(0, nsize)
+    vectoruser = data.get(usuario)
+
+    for i in range(0, nsize):
+        tmpuser = i
+        if tmpuser != usuario:
+            tmpvector = data.get(tmpuser)
+            if not (tmpvector is None):
+              tmpdist = distancia(vectoruser, tmpvector)
+              if tmpdist is not math.nan:
+                listDist, listName = keepClosest(funName, listDist, listName, tmpdist, tmpuser, N)
+
+    return listDist, listName
+
+# Find the K closest firsts Item recommendation
+def recommendationL(usuario, distancia, N, items, minr, data):
+    ldistK, luserK = knn_L(N, distancia, usuario, data)
+
+    user = data.get(usuario)
+    recom = [None] * N
+    for i in range(0, N):
+        recom[i] = data.get(luserK[i])
+    # print('user preference:', user)
+
+    lstRecomm = [-1] * items
+    lstUser = [None] * items
+    lstObj = [None] * items
+    k = 0
+
+    fullObjs = {}
+    count = 0
+    for i in recom:
+        for j in i:
+          tmp = fullObjs.get(j)
+          if tmp is None:
+            fullObjs[j] = [i.get(j), luserK[count]]
+          else:
+            nval = i.get(j)
+            if nval > tmp[0]:
+              fullObjs[j] = [nval, luserK[count]]
+        count += 1
+
+    finallst = topSuggestions(fullObjs, count, items)
+    return finallst
+
+def topSuggestions(fullObj, k, items):
+  rp = [-1]*items
+
+  for i in fullObj:
+    rating = fullObj.get(i)
+
+    for j in range(0, items):
+      if rp[j] == -1 :
+        tmp = [i, rating[0], rating[1]]
+        rp.insert(j, tmp)
+        rp.pop()
+        break
+      else:
+        tval = rp[j]
+        if tval[1] < rating[0]:
+          tmp = [i, rating[0], rating[1]]
+          rp.insert(j, tmp)
+          rp.pop()
+          break
+
+  return rp
+
+lstdb20 = readLargeFileDask('ml-20m/ratings.csv') # 54 seg
+
+usuario = 45600
+rfunc = euclidianaL
+
+# 10 vecinos, 20 recomendaciones
+lista = recommendationL(usuario, rfunc, 10, 20, 3.0, lstdb20) # 3 seg
+for i in lista:
+    print('user:', i[2], 'obj:',i[0], 'rating:', i[1])
+
+################################################################
+
 @app.route("/", methods=['POST', 'GET'])
 def distancias():
     if request.method == 'POST':
         user_1 = request.form['option_a']
         user_2 = request.form['option_b']
-        distancia_manhattan = str(manhattan(users[user_1], users[user_2])) 
-        distancia_pearson = str(pearson_correlation(users[user_1], users[user_2]))
+        recomendacion_20m = str(lista) 
         # Simplemente imprimir los valores en la consola
-        print(f"Distancia Manhattan entre {user_1} y {user_2}: {distancia_manhattan}")
-        print(f"Coeficiente de correlación de Pearson entre {user_1} y {user_2}: {distancia_pearson}")
+        print(f"Recomendacion 20 Millones: {recomendacion_20m}")
 
     return render_template('index.html')
 
